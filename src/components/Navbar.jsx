73f14dfffb4da989b2f0112e60e9160f1e0b75c9
singhankita29 <100141535+singhankita29@users.noreply.github.com>
2022-05-24 18:26:27 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Navbar = () => {
+ //return <h1>Navbar</h1>
+  return (
+       <div class="navfirst">
+         <div class="container">
+            <div class="box1">
+                <h1>LOGOBAKERY</h1>
+            </div>
+            <div class="box2">
+               <h3>Services</h3>
+               <h3>Project</h3>
+               <h3>About</h3>
+            </div>
+            <div class="box3">
+                <button>Contact</button>
+            </div>
+         </div>
+       </div>
+  )
+};
+
+export default Navbar;
